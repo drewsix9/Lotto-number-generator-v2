6380142bb274be5c94b87ccd4894690aa9e66728
drewsix9 <145753941+drewsix9@users.noreply.github.com>
2024-01-23 20:26:57 +0800

    Add Lotto class instance and update HistoryTilesBuilder

--- lib/widgets/HistoryTiles.dart
+++ lib/widgets/HistoryTiles.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lotto_number_generator/pages/642.dart';
 import 'package:lotto_number_generator/utils/bloc/DataBase.dart';
+import 'package:lotto_number_generator/utils/models/Lotto_class.dart';
 import 'package:provider/provider.dart';
+
+Lotto lotto = Lotto(); // instance of Lotto class
 
 Widget HistoryTilesBuilder(BuildContext context, int index) {
   return Consumer<LottoDataBase>(
     builder: (context, value, child) => ListTile(
+      // int i = v
       leading: Image.asset(
         'assets/images/642.png',
         width: 50,
